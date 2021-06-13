8c21bff0f25a32e2943264b3515f7942fcf18621
simonojok19 <simonojok19@gmail.com>
2021-06-13 11:47:49 +0300

    installed the fonts

--- Header.js
+++ Header.js
@@ -1,3 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View, Image} from "react-native";
 import logo from './assets/globomantics-logo-bug-black.png'
+
+export default function Header({headerDisplay}) {
+    return(
+        <View styles={styles.header}>
+            <Image source={logo} style={{width: 35, height: 35}}/>
+            <View>
+                <Text style={styles.text}>{headerDisplay}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontFamily: 'OpenSans',
+    }
+});
